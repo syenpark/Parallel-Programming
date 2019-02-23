27d88ea72da7ea74ae018bfe897f38c519a73440
Syen Park <syenpark@gmail.com>
2019-02-23 17:41:21 +0800

    add hw

---
+++ src/HW/assignment1-package/mpi_push_relabel_skeleton.cpp
@@ -0,0 +1,19 @@
+/**
+ * Name:
+ * Student id:
+ * ITSC email:
+*/
+
+
+#include "mpi_push_relabel.h"
+
+/*
+ *  You can add helper functions and variables as you wish.
+ */
+
+int push_relabel(int my_rank, int p, MPI_Comm comm, int N, int src, int sink, int *cap, int *flow) {
+    /*
+     *  Please fill in your codes here.
+     */
+    return 0;
+}
