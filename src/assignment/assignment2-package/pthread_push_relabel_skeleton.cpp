225257ff587cb67fa1897d575992247f21d735fe
Syen Park <syenpark@gmail.com>
2019-03-23 02:02:30 +0800

    Start A02-PThread

--- src/assignment/assignment2-package/pthread_push_relabel_skeleton.cpp
+++ src/assignment/assignment2-package/pthread_push_relabel_skeleton.cpp
@@ -3,7 +3,12 @@
  * Student id:
  * ITSC email:
 */
+#include <cstring>
+#include <cstdint>
+#include <cstdlib>
 
+#include <vector>
+#include <iostream>
 
 #include "pthread_push_relabel.h"
 #include <pthread.h>
@@ -11,13 +16,28 @@
 /*
  *  You can add helper functions and variables as you wish.
  */
+using namespace std;
+
+void pre_flow(int *dist, int64_t *excess, int *cap, int *flow, int N, int src) {
+    dist[src] = N;
+    for (auto v = 0; v < N; v++) {
+        flow[utils::idx(src, v, N)] = cap[utils::idx(src, v, N)];
+        flow[utils::idx(v, src, N)] = -flow[utils::idx(src, v, N)];
+        excess[v] = flow[utils::idx(src, v, N)];
+    }
+}
 
 int push_relabel(int num_threads, int N, int src, int sink, int *cap, int *flow) {
     /*
      *  Please fill in your codes here.
      */
     // all things into threds
+    long       thread;  /* Use long in case of a 64-bit system */
+    pthread_t* thread_handles;
 
+    thread_handles = (pthread_t*) malloc (num_threads*sizeof(pthread_t));
+
+    free(thread_handles);
 
     // sequential
     // two for loop into one for loop
@@ -25,6 +45,87 @@ int push_relabel(int num_threads, int N, int src, int sink, int *cap, int *flow)
     // syncrhonization between step 1 and 2
 
     // pthread creat function
+    /*
+    int *dist = (int *) calloc(N, sizeof(int));
+    int *stash_dist = (int *) calloc(N, sizeof(int));
+    auto *excess = (int64_t *) calloc(N, sizeof(int64_t));
+    auto *stash_excess = (int64_t *) calloc(N, sizeof(int64_t));
 
+    // PreFlow.
+    pre_flow(dist, excess, cap, flow, N, src);
+
+    vector<int> active_nodes;
+    int *stash_send = (int *) calloc(N * N, sizeof(int));
+    for (auto u = 0; u < N; u++) {
+        if (u != src && u != sink) {
+            active_nodes.emplace_back(u);
+        }
+    }
+
+    // Four-Stage Pulses.
+    while (!active_nodes.empty()) {
+        // Stage 1: push.
+        for (auto u : active_nodes) {
+            for (auto v = 0; v < N; v++) {
+                auto residual_cap = cap[utils::idx(u, v, N)] -
+                                    flow[utils::idx(u, v, N)];
+                if (residual_cap > 0 && dist[u] > dist[v] && excess[u] > 0) {
+                    stash_send[utils::idx(u, v, N)] = std::min<int64_t>(excess[u], residual_cap);
+                    excess[u] -= stash_send[utils::idx(u, v, N)];
+                }
+            }
+        }
+        for (auto u : active_nodes) {
+            for (auto v = 0; v < N; v++) {
+                if (stash_send[utils::idx(u, v, N)] > 0) {
+                    flow[utils::idx(u, v, N)] += stash_send[utils::idx(u, v, N)];
+                    flow[utils::idx(v, u, N)] -= stash_send[utils::idx(u, v, N)];
+                    stash_excess[v] += stash_send[utils::idx(u, v, N)];
+                    stash_send[utils::idx(u, v, N)] = 0;
+                }
+            }
+        }
+
+        // Stage 2: relabel (update dist to stash_dist).
+        memcpy(stash_dist, dist, N * sizeof(int));
+        for (auto u : active_nodes) {
+            if (excess[u] > 0) {
+                int min_dist = INT32_MAX;
+                for (auto v = 0; v < N; v++) {
+                    auto residual_cap = cap[utils::idx(u, v, N)] - flow[utils::idx(u, v, N)];
+                    if (residual_cap > 0) {
+                        min_dist = min(min_dist, dist[v]);
+                        stash_dist[u] = min_dist + 1;
+                    }
+                }
+            }
+        }
+
+        // Stage 3: update dist.
+        swap(dist, stash_dist);
+
+        // Stage 4: apply excess-flow changes for destination vertices.
+        for (auto v = 0; v < N; v++) {
+            if (stash_excess[v] != 0) {
+                excess[v] += stash_excess[v];
+                stash_excess[v] = 0;
+            }
+        }
+
+        // Construct active nodes.
+        active_nodes.clear();
+        for (auto u = 0; u < N; u++) {
+            if (excess[u] > 0 && u != src && u != sink) {
+                active_nodes.emplace_back(u);
+            }
+        }
+    }
+
+    free(dist);
+    free(stash_dist);
+    free(excess);
+    free(stash_excess);
+    free(stash_send);
+    */
     return 0;
 }
