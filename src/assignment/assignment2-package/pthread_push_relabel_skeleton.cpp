3e7da77ce48a35524515152914589751eb8472a1
Syen Park <syenpark@gmail.com>
2019-03-22 19:11:39 +0800

    gitflow-feature-stash: A02

--- src/assignment/assignment2-package/pthread_push_relabel_skeleton.cpp
+++ src/assignment/assignment2-package/pthread_push_relabel_skeleton.cpp
@@ -15,5 +15,15 @@ int push_relabel(int num_threads, int N, int src, int sink, int *cap, int *flow)
     /*
      *  Please fill in your codes here.
      */
+    // all things into threds
+
+
+    // sequential
+    // two for loop into one for loop
+
+    // syncrhonization between step 1 and 2
+
+    // pthread creat function
+
     return 0;
 }
