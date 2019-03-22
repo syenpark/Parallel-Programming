5a274a2cac91bf37fe0c2f7e107573605320b73d
Syen Park <syenpark@gmail.com>
2019-03-22 20:55:52 +0800

    comment hello example

--- src/example/ch4/pth_hello.c
+++ src/example/ch4/pth_hello.c
@@ -30,30 +30,49 @@ void *Hello(void* rank);  /* Thread function */
 /*--------------------------------------------------------------------*/
 int main(int argc, char* argv[]) {
    long       thread;  /* Use long in case of a 64-bit system */
-   pthread_t* thread_handles; 
+   pthread_t* thread_handles;
+
+   // If not <program_name> <the # of threads> format, print the alert message
+   if (argc != 2) Usage(argv[0]);
 
    /* Get number of threads from command line */
-   if (argc != 2) Usage(argv[0]);
-   thread_count = strtol(argv[1], NULL, 10);  
+   thread_count = strtol(argv[1], NULL, 10);
+
+   // If the number of threads is not proper, print the alert message
    if (thread_count <= 0 || thread_count > MAX_THREADS) Usage(argv[0]);
 
-   thread_handles = malloc (thread_count*sizeof(pthread_t)); 
+   thread_handles = malloc (thread_count*sizeof(pthread_t));
+
+   /*
+    * pthread.h -> pthread_t: One object for each thread
+    * int pthread_create(pthread_t* thread_p, const pthread_attr_t* attr_p, void, void);
+    *                         out                        in                  in    in
+    *
+    * pthread_t* thread_p should be allocated before calling by above code
+    * const pthread_attr_t* attr_p; We won't be using, so we just pass NULL
+    * The function that the read is to run
+    * Pointer to the argument that should be passed to the function start_routine
+    */
 
    for (thread = 0; thread < thread_count; thread++)  
-      pthread_create(&thread_handles[thread], NULL,
-          Hello, (void*) thread);  
+      pthread_create(&thread_handles[thread], NULL, Hello, (void*) thread);
 
    printf("Hello from the main thread\n");
 
+   // Stopping the threads
    for (thread = 0; thread < thread_count; thread++) 
       pthread_join(thread_handles[thread], NULL); 
 
    free(thread_handles);
+
    return 0;
 }  /* main */
 
 /*-------------------------------------------------------------------*/
 void *Hello(void* rank) {
+   /*
+    * Print greeting message per each thread
+    */
    long my_rank = (long) rank;  /* Use long in case of 64-bit system */ 
 
    printf("Hello from thread %ld of %d\n", my_rank, thread_count);
@@ -63,6 +82,9 @@ void *Hello(void* rank) {
 
 /*-------------------------------------------------------------------*/
 void Usage(char* prog_name) {
+   /*
+    * Print alert message about invalid input (argument style or the number of it)
+    */
    fprintf(stderr, "usage: %s <number of threads>\n", prog_name);
    fprintf(stderr, "0 < number of threads <= %d\n", MAX_THREADS);
    exit(0);
