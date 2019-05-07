ce3eacbffa4c4ada3977ca74eccbbc3ab2be29df
Syen Park <syenpark@gmail.com>
2019-05-07 21:36:52 +0800

    refactor

--- src/example/deviceVariable.cu
+++ src/example/deviceVariable.cu
@@ -2,20 +2,28 @@
 #include <stdlib.h>
 
 __device__ int d_value;
+
+// Device code: GPU function
 __global__ void test_Kernel()
 {
 	int threadID = threadIdx.x;
 	d_value = 1;
-	printf("threadID %-3d d_value%3d\n",threadID,d_value);
+
+	printf("threadID %-3d d_value%3d\n", threadID, d_value);
 }
+
+// Host code: CPU function
 int main()
 {
 	int h_value = 0;
-	test_Kernel<<<1,2>>>();
-	cudaMemcpyFromSymbol(&h_value,d_value,
-		sizeof(int),0,cudaMemcpyDeviceToHost);
+
+    // kernelName<<<#block_per_grid, #thread_per_block, shared_size, s>>>(param1, ...);
+	test_Kernel<<<1, 2>>>();
+
+	cudaMemcpyFromSymbol(&h_value, d_value, sizeof(int), 0, cudaMemcpyDeviceToHost);
 	
 	printf("Output from host: %d\n",h_value);
+
 	return 0;
 }
 
