5308c42d780d785f7d5ad803e8c7ab634e2f8fdb
Syen Park <syenpark@gmail.com>
2019-05-08 00:22:49 +0800

    Rename host pointers

--- src/assignment/assignment3-package/cuda_push_relabel_skeleton.cu
+++ src/assignment/assignment3-package/cuda_push_relabel_skeleton.cu
@@ -26,19 +26,38 @@ void pre_flow(int *dist, int64_t *excess, int *cap, int *flow, int N, int src) {
 }
 
 int push_relabel(int blocks_per_grid, int threads_per_block, int N, int src, int sink, int *cap, int *flow) {
-    int threadsPerBlock = 256;
-    int blocksPerGrid = N / threadsPerBlock;
+    size_t size_in_int = N * sizeof(int);
+    size_t size_n_int64_t = N * sizeof(int64_t);
 
-    int *dist = (int *) calloc(N, sizeof(int));
-    int *stash_dist = (int *) calloc(N, sizeof(int));
-    auto *excess = (int64_t *) calloc(N, sizeof(int64_t));
-    auto *stash_excess = (int64_t *) calloc(N, sizeof(int64_t));
+    // Allocate Vectors in host memory (CPU)
+    int *h_dist = (int *) calloc(N, sizeof(int));
+    int *h_stash_dist = (int *) calloc(N, sizeof(int));
+    auto *h_excess = (int64_t *) calloc(N, sizeof(int64_t));
+    auto *h_stash_excess = (int64_t *) calloc(N, sizeof(int64_t));
+
+    //printf("%d %d\n", sizeof(int), sizeof(int64_t));
+    /*
+    // Allocate Vectors in device memory (GPU)
+    int *d_dist = (int *) calloc(N, sizeof(int));
+    int *d_stash_dist = (int *) calloc(N, sizeof(int));
+    auto *d_excess = (int64_t *) calloc(N, sizeof(int64_t));
+    auto *d_stash_excess = (int64_t *) calloc(N, sizeof(int64_t));
+    int *d_stash_send = (int *) calloc(N * N, sizeof(int));
+
+    cudaMalloc((void**) &d_dist, size);
+    cudaMalloc((void**) &d_stash_dist, size);
+    cudaMalloc((void**) &d_excess, size);
+    cudaMalloc((void**) &d_stash_excess, size);
+    cudaMalloc((void**) &d_stash_send, size);
+     */
+
 
     // PreFlow.
-    pre_flow(dist, excess, cap, flow, N, src);
+    pre_flow(h_dist, h_excess, cap, flow, N, src);
 
     vector<int> active_nodes;
     int *stash_send = (int *) calloc(N * N, sizeof(int));
+
     for (auto u = 0; u < N; u++) {
         if (u != src && u != sink) {
             active_nodes.emplace_back(u);
@@ -52,9 +71,9 @@ int push_relabel(int blocks_per_grid, int threads_per_block, int N, int src, int
             for (auto v = 0; v < N; v++) {
                 auto residual_cap = cap[utils::idx(u, v, N)] -
                                     flow[utils::idx(u, v, N)];
-                if (residual_cap > 0 && dist[u] > dist[v] && excess[u] > 0) {
-                    stash_send[utils::idx(u, v, N)] = std::min<int64_t>(excess[u], residual_cap);
-                    excess[u] -= stash_send[utils::idx(u, v, N)];
+                if (residual_cap > 0 && h_dist[u] > h_dist[v] && h_excess[u] > 0) {
+                    stash_send[utils::idx(u, v, N)] = std::min<int64_t>(h_excess[u], residual_cap);
+                    h_excess[u] -= stash_send[utils::idx(u, v, N)];
                 }
             }
         }
@@ -63,51 +82,51 @@ int push_relabel(int blocks_per_grid, int threads_per_block, int N, int src, int
                 if (stash_send[utils::idx(u, v, N)] > 0) {
                     flow[utils::idx(u, v, N)] += stash_send[utils::idx(u, v, N)];
                     flow[utils::idx(v, u, N)] -= stash_send[utils::idx(u, v, N)];
-                    stash_excess[v] += stash_send[utils::idx(u, v, N)];
+                    h_stash_excess[v] += stash_send[utils::idx(u, v, N)];
                     stash_send[utils::idx(u, v, N)] = 0;
                 }
             }
         }
 
         // Stage 2: relabel (update dist to stash_dist).
-        memcpy(stash_dist, dist, N * sizeof(int));
+        memcpy(h_stash_dist, h_dist, N * sizeof(int));
         for (auto u : active_nodes) {
-            if (excess[u] > 0) {
+            if (h_excess[u] > 0) {
                 int min_dist = INT32_MAX;
                 for (auto v = 0; v < N; v++) {
                     auto residual_cap = cap[utils::idx(u, v, N)] - flow[utils::idx(u, v, N)];
                     if (residual_cap > 0) {
-                        min_dist = min(min_dist, dist[v]);
-                        stash_dist[u] = min_dist + 1;
+                        min_dist = min(min_dist, h_dist[v]);
+                        h_stash_dist[u] = min_dist + 1;
                     }
                 }
             }
         }
 
         // Stage 3: update dist.
-        swap(dist, stash_dist);
+        swap(h_dist, h_stash_dist);
 
         // Stage 4: apply excess-flow changes for destination vertices.
         for (auto v = 0; v < N; v++) {
-            if (stash_excess[v] != 0) {
-                excess[v] += stash_excess[v];
-                stash_excess[v] = 0;
+            if (h_stash_excess[v] != 0) {
+                h_excess[v] += h_stash_excess[v];
+                h_stash_excess[v] = 0;
             }
         }
 
         // Construct active nodes.
         active_nodes.clear();
         for (auto u = 0; u < N; u++) {
-            if (excess[u] > 0 && u != src && u != sink) {
+            if (h_excess[u] > 0 && u != src && u != sink) {
                 active_nodes.emplace_back(u);
             }
         }
     }
 
-    free(dist);
-    free(stash_dist);
-    free(excess);
-    free(stash_excess);
+    free(h_dist);
+    free(h_stash_dist);
+    free(h_excess);
+    free(h_stash_excess);
     free(stash_send);
 
     return 0;
