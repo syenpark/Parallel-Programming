5fac5fb104df115d8cec3adc0b09b2cebdd32da7
Syen Park <syenpark@gmail.com>
2019-05-08 01:13:18 +0800

    copy data

--- src/assignment/assignment3-package/cuda_push_relabel_skeleton.cu
+++ src/assignment/assignment3-package/cuda_push_relabel_skeleton.cu
@@ -25,6 +25,45 @@ void pre_flow(int *dist, int64_t *excess, int *cap, int *flow, int N, int src) {
     }
 }
 
+// Device code
+__global__ void foo_kernel(int* A, int* B, int* C)
+{
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    C[i] = A[i] + B[i];
+}
+/*
+__global__ void push(int *active_nodes, int *active_node_size, int N, int *cap, int *flow, int *dist, unsigned long long *excess, int *stash_send){
+    int blockId = blockIdx.x;
+    int threadId = threadIdx.x;
+    int numBlock = gridDim.x;
+    int numThread = blockDim.x;
+    int v, u;
+    extern __shared__ unsigned long long residual_cap[];
+
+    for (int i = blockId; i < active_node_size[0]; i+=numBlock){
+        u = active_nodes[i];
+
+        for (v = threadId; v < N; v+=numThread){
+            residual_cap[v] = cap[idx(u, v, N)] - flow[idx(u, v, N)];
+        }
+        __syncthreads();
+
+        if (threadId == 0){
+            v = 0;
+
+            while(excess[u]>0 && v < N){
+                if (residual_cap[v] > 0 && dist[u] > dist[v]){
+                    stash_send[idx(u, v, N)] = min(excess[u], residual_cap[v]);
+                    excess[u] -= stash_send[idx(u, v, N)];
+                }
+                v++;
+            }
+        }
+        __syncthreads();
+    }
+}
+ */
+
 int push_relabel(int blocks_per_grid, int threads_per_block, int N, int src, int sink, int *cap, int *flow) {
     size_t size_in_int = N * sizeof(int);
     size_t size_n_int64_t = N * sizeof(int64_t);
@@ -34,6 +73,8 @@ int push_relabel(int blocks_per_grid, int threads_per_block, int N, int src, int
     int *h_stash_dist = (int *) calloc(N, sizeof(int));
     auto *h_excess = (int64_t *) calloc(N, sizeof(int64_t));
     auto *h_stash_excess = (int64_t *) calloc(N, sizeof(int64_t));
+    vector<int> h_active_nodes;
+    int *h_stash_send = (int *) calloc(N * N, sizeof(int));
 
     //printf("%d %d\n", sizeof(int), sizeof(int64_t));
 
@@ -51,23 +92,27 @@ int push_relabel(int blocks_per_grid, int threads_per_block, int N, int src, int
     // PreFlow.
     pre_flow(h_dist, h_excess, cap, flow, N, src);
 
-    // Copy vectors from host memory to device memory
-    cudaMemcpy(d_dist, h_dist, size_in_int, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_excess, h_excess, size_n_int64_t, cudaMemcpyHostToDevice);
-
-    vector<int> active_nodes;
-    int *h_stash_send = (int *) calloc(N * N, sizeof(int));
-
     for (auto u = 0; u < N; u++) {
         if (u != src && u != sink) {
-            active_nodes.emplace_back(u);
+            h_active_nodes.emplace_back(u);
         }
     }
 
+    // Copy vectors from host memory to device memory
+    cudaMemcpy(d_dist, h_dist, size_in_int, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_excess, h_excess, size_n_int64_t, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_stash_dist, h_stash_dist, size_in_int, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_stash_send, h_stash_send, size_in_int, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_stash_excess, h_stash_excess, size_n_int64_t, cudaMemcpyHostToDevice);
+
     // Four-Stage Pulses.
-    while (!active_nodes.empty()) {
+    while (!h_active_nodes.empty()) {
         // Stage 1: push.
-        for (auto u : active_nodes) {
+        //foo_kernel<<<blocks_per_grid, threads_per_block>>>(d_dist, d_excess);
+        //push<<<blocks_per_grid, threads_per_block>>>(h_active_nodes, active_node_size, N, cap, flow, dist, excess, d_stash_send);
+
+
+        for (auto u : h_active_nodes) {
             for (auto v = 0; v < N; v++) {
                 auto residual_cap = cap[utils::idx(u, v, N)] - flow[utils::idx(u, v, N)];
 
@@ -85,9 +130,12 @@ int push_relabel(int blocks_per_grid, int threads_per_block, int N, int src, int
             }
         }
 
+
+        cudaMemcpy(h_stash_send, d_stash_send, size_in_int, cudaMemcpyDeviceToHost);
+
         // Stage 2: relabel (update dist to stash_dist).
         memcpy(h_stash_dist, h_dist, N * sizeof(int));
-        for (auto u : active_nodes) {
+        for (auto u : h_active_nodes) {
             if (h_excess[u] > 0) {
                 int min_dist = INT32_MAX;
                 for (auto v = 0; v < N; v++) {
@@ -112,10 +160,10 @@ int push_relabel(int blocks_per_grid, int threads_per_block, int N, int src, int
         }
 
         // Construct active nodes.
-        active_nodes.clear();
+        h_active_nodes.clear();
         for (auto u = 0; u < N; u++) {
             if (h_excess[u] > 0 && u != src && u != sink) {
-                active_nodes.emplace_back(u);
+                h_active_nodes.emplace_back(u);
             }
         }
     }
